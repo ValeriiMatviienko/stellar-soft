3eeb966c1a8d7ffa1b2eb071b60af512f69c86e1
ValeriiMatviienko <unanimous276@gmail.com>
2021-08-19 20:16:04 +0300

    add circle

--- script.js
+++ script.js
@@ -5,17 +5,15 @@ const container = document.getElementById("container");
 open.addEventListener("click", () => {
   container.classList.add("active");
 });
-
 close.addEventListener("click", () => {
   container.classList.remove("active");
 });
 
-/* есть функция, у которой в качестве аргументов строки '*', '2', 's', '2d', реализуйте ее так что бы вернулась строка '2*s*2d' */
+/* есть функция, у которой в качестве аргументов строки '*', '2', 's', '2d',
+ реализуйте ее так чтобы вернулась строка '2*s*2d' 
+ */
 
 function getStr(...arg) {
   return arg.slice(1).join(arg[0]);
 }
-
-// function str() {
-//   return [].slice.call(arguments, 1).join(arguments[0]);
-// }
+console.log(getStr("*", "2", "s", "2d"));
