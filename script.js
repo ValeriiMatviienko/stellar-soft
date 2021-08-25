7752840fd2df2158fcaa62a2279a695db2b0e91a
ValeriiMatviienko <unanimous276@gmail.com>
2021-08-25 22:44:21 +0300

    scroll disable

--- script.js
+++ script.js
@@ -4,16 +4,25 @@ const container = document.getElementById("container");
 
 open.addEventListener("click", () => {
   container.classList.add("active");
+  document.body.style.overflow = "hidden";
 });
 close.addEventListener("click", () => {
   container.classList.remove("active");
+  document.body.style.overflow = "scroll";
 });
 
-/* есть функция, у которой в качестве аргументов строки '*', '2', 's', '2d',
- реализуйте ее так чтобы вернулась строка '2*s*2d' 
- */
+/***************************************************/
 
 function getStr(...arg) {
   return arg.slice(1).join(arg[0]);
 }
 console.log(getStr("*", "2", "s", "2d"));
+
+/***************************************************/
+
+//   if (open) {
+//     document.body.style.overflow = "hidden";
+//   } else {
+//     document.body.style.overflow = "visible";
+//   }
+// };
